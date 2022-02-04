5ee64e9ee8a693209d4d130fa16b4683bd5847ad
OlhaNester <olga_nv@ukr.net>
2022-02-04 18:03:49 +0200

    222

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import {Layout} from './components/Layout/Layout';
+;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />}/>
+        <Route path="movies" element={<MoviesPage />} >
+          <Route path="/:movieId" element={<MovieDetailsPage />}>
+            <Route path="/cast" element={<Cast />}/>
+            <Route path="/reviews" element={<Reviews />}/>
+          </Route>
+        </Route>
+        </Route>
+        
+      </Routes>
+    
   );
 }
 
+
+function HomePage() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Outlet/>
+    </>
+  );
+}
+
+function MoviesPage() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+        <Outlet/>
+    </>
+  );
+}
+
+function MovieDetailsPage() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Outlet/>
+    </>
+  );
+}
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+export const Layout = () => {
+    return (
+        <>
+        <NavLink to="/">Home</NavLink>
+        
+        <NavLink to="/Movies">Movies</NavLink>
+        </>
+    );
+} ;
