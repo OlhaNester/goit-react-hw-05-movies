a1fe3c58fe6d4dbec6d1c6789892d95c430bd209
OlhaNester <olga_nv@ukr.net>
2022-02-16 22:19:52 +0200

    888

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import { AppContainer } from "./App.styled";
-import Layout from "./components/Layout/Layout";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { MovieDetailsPage } from "./pages/MovieDetailsPage/MovieDetailsPage";
-import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
+//import Layout from "./components/Layout/Layout";
+//import { HomePage } from "./pages/HomePage/HomePage";
+//import { MovieDetailsPage } from "./pages/MovieDetailsPage/MovieDetailsPage";
+//import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
 import { Cast } from "./pages/Cast/Cast";
 import { Reviews } from "./pages/Reviews/Reviews";
+
+const loader = (componentName) => {
+  return lazy(() => import(`./pages/${componentName}/${componentName}`));
+};
+const Layout = loader("Layout");
+const HomePage = loader("HomePage");
+const MoviesPage = loader("MoviesPage");
+const MovieDetailsPage = loader("MovieDetailsPage");
 
 function App() {
   <GlobalStyle />;
   return (
     <AppContainer>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />}></Route>
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback="">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />}></Route>
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </AppContainer>
   );
 }
